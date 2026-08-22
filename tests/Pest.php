ddf6a443ac5c9f505243853a3d43c54060fb1db7
Tanzar <spakowski19@gmail.com>
2026-08-22 17:32:04 +0200

    pipeline fix 4

--- tests/Pest.php
+++ tests/Pest.php
@@ -5,4 +5,4 @@ declare(strict_types=1);
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tanzar\Refract\Tests\TestCase;
 
-uses(TestCase::class, RefreshDatabase::class)->in(__DIR__);
+uses(TestCase::class, RefreshDatabase::class)->in('Unit', 'Feature');
